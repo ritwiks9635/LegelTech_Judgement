da9e00bf86b4d9877f5e281291fafbccda0c22bd
Ritwik Sarkar <132639994+ritwiks9635@users.noreply.github.com>
2025-12-14 17:26:14 +0530

    Update test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,4 +1,4 @@
 # from app.parser import parse_pdf
 # pdf_path = "data\pdfs\S_N_Enterprises_vs_Income_Tax_Officer_Ors_on_27_September_2022 (1).PDF"
-# GOOGLE_API_KEY = "AIzaSyBMdl-4KejVan3wP8teTnxZNIzjnRBgf-I"
+# GOOGLE_API_KEY = "Api_key"
 # meta = parse_pdf(pdf_path, GOOGLE_API_KEY)
